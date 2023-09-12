ebe1b8cdf860d21d948edf9cea5becfeda7394e0
alexsilva <geniofuturo@gmail.com>
2023-09-12 11:44:51 -0300

    v1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='favicon-generator',
-	version='1.0',
+	version='1.1',
 	py_modules=['faviconGenerator'],
 	url='https://github.com/Hecsall/favicon-generator',
 	scripts=['scripts/favicon_generator.py'],
